96ce35d64b982a52ffe981be14c8e547f794855d
Amrit marasini <amritmarasini163@gmail.com>
2024-02-08 12:51:45 +0545

    initial commit

---
+++ counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter]  =useState(15)
+
+  //let counter = 15
+  const addValue = () =>{
+    console.log("clicked",Math.counter())
+    //counter = counter + 1
+    setCounter(counter + 1)
+  }
+  const removeValue = () =>{
+    setCounter(counter - 1)
+  }
+  
+
+  return (
+    <>
+     <h1>Number Increasing Or Decrising</h1>
+     <h2>Counter value: {counter}</h2>
+
+     <button
+     onClick={addValue}>
+      Add value{counter}</button>
+     <br/>
+     <button
+     onClick={removeValue}
+     
+     >remove value{counter}</button>
+
+     <p>footer: {counter}</p>
+     
+    </>
+  )
+}
+
+export default App
